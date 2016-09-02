da286c534ef4b9f99493833641ba60d28ac1320f
wabray17 <wabray17@gmail.com>
2016-09-01 21:22:16 -0500

    Class work

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,9 +1,42 @@
 class MonstersController < ApplicationController
-  def cthulu
-    @monster = Monster.find(3)
+  def index
+    @page_title = "All of the monsters"
+    @monsters = Monster.all
   end
 
-  def index
-    @monsters = Monster.all
+  def show
+    @page_title = "Monster!"
+    @monster = Monster.find(params[:id])
+  end
+
+  def new
+    @page_title = "Add monster"
+    p "**********************************************"
+  end
+
+  def create
+    @page_title = "Added monster"
+    @monster = Monster.create(name: params[:name], 
+                              origin: params[:origin], 
+                              price: params[:price], 
+                              description: params[:description], 
+                              image: params[:image],
+                              danger_rating: params[:danger])
+    render 'show.html.erb'
+  end
+
+  def edit 
+    @monster = Monster.find(params[:id])
+  end
+
+  def update
+    @monster = Monster.find(params[:id])
+    @monster.update(name: params[:name],
+                    origin: params[:origin],
+                    price: params[:price],
+                    description: params[:description],
+                    image: params[:image],
+                    danger_rating: params[:danger_rating])
+    render 'show.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get '/favorite_monster' => 'monsters#cthulu'
-  get '/all_monsters' => 'monsters#index'
+  get '/' => 'monsters#index'
+  get '/monsters' => 'monsters#index'
+  get '/monsters/new' => 'monsters#new'
+  post '/monsters' => 'monsters#create'
+  get '/monsters/:id' => 'monsters#show'
+  get 'monsters/:id/edit' => 'monsters#edit'
+  patch 'monsters/:id' => 'monsters#update'
 end
