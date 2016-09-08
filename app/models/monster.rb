56b32945083e70bb75e63c176d41d928641ca056
wabray17 <wabray17@gmail.com>
2016-09-07 20:29:41 -0500

    classwork, without dispalying

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,17 @@
 class Monster < ApplicationRecord
+  def sale_message
+    if price <= 2 
+      message = "Discount monster!"
+    else
+      message = "This monster is for sale."
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total 
+    (price + tax).round(2)
+  end
 end
